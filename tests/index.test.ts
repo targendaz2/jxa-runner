cebb154303a2ff165f770be43211f4c46d7cbe47
David G Rosenberg <targendaz2@me.com>
2024-06-01 22:23:57 -0400

    Can serialize JXA function with arguments

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -6,4 +6,8 @@ describe('serialization tests', () => {
     test('can serialize JXA function', () => {
         expect(run(() => 'hello')).toEqualCode('() => "hello"');
     });
+
+    test('can serialize JXA function with arguments', () => {
+        expect(run((value: string) => value)).toEqualCode('(value) => value');
+    });
 });
