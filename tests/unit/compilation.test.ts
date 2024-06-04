d71f576b15e75d273a6d4b258225f5f76bdbd63b
David G Rosenberg <targendaz2@me.com>
2024-06-03 23:51:07 -0400

    compilation tests happen in virtual file system

--- tests/unit/compilation.test.ts
+++ tests/unit/compilation.test.ts
@@ -1,14 +1,16 @@
+import fs from 'node:fs';
 import { describe, expect, test } from '@jest/globals';
+import { fs as memfs, vol } from 'memfs';
+import { ufs } from 'unionfs';
 import '../../jest.setup';
-import config from '../../src/config/common.js';
 import { JxaCompiler, compile } from '../../src/lib/compilers.js';
-import { outputTemplate } from '../../src/lib/output.js';
 import {
     ArgsSerializer,
     FnSerializer,
     serialize,
 } from '../../src/lib/serializers.js';
 import { JxaCodeTemplate, fillTemplate } from '../../src/lib/templates.js';
+import packageFactory from '../factories/package.factory';
 
 describe('JXA compilation tests', () => {
     test('can compile JXA code', async () => {
@@ -22,13 +24,23 @@ describe('JXA compilation tests', () => {
             fn: serializedFn,
             args: serializedArgs,
         });
-        outputTemplate(code);
 
-        await compile(JxaCompiler, {
-            entryPath: config.entryPath,
-            outputPath: config.outputPath,
+        const packageStructure = packageFactory.build({
+            './src/index.js': code,
         });
 
-        expect(config.outputPath).toExist();
+        vol.fromJSON(packageStructure, '/jxa-runner-test');
+        // @ts-expect-error 'need to cast memfs as the correct type'
+        ufs.use(fs).use(memfs);
+
+        await compile(JxaCompiler, {
+            entryPath: '/jxa-runner-test/src/index.js',
+            outputPath: '/jxa-runner-test/.tmp/dist/bundle.js',
+            filesystem: ufs,
+        });
+
+        expect(
+            ufs.existsSync('/jxa-runner-test/.tmp/dist/bundle.js'),
+        ).toBeTruthy();
     });
 });
